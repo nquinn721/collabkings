487c880a4176f8c5c083cadfeb2f5df6fec01af0
Nathan <natethepcspecialist@gmail.com>
2015-02-22 22:58:20 -0500

    update

--- client/public/js/file.js
+++ client/public/js/file.js
@@ -41,9 +41,7 @@ File.prototype = {
 		return false;
 	},
 	getParents : function () {
-		console.log(this.file);
-		console.log(this.file.indexOf("\\"));
-		if(this.file.match(/\\/)){
+		if(this.file.match(/\\|\//)){
 			var f = this.file.split('\\');
 			console.log(f, f[f.length - 1])
 			this.file = f[f.length - 1];
